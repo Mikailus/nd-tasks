4468b0e9c1342b20fe7a4b5eb819dcf0ea0b9d85
Michał Wajer <wajer.michael@gmail.com>
2020-10-04 11:00:12 +0200

    Tasks

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from '../user.interface';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+  @Input() user: User;
+  @Output() delete: EventEmitter<number> = new EventEmitter<number>();
+
+onDeleteButtonClick(): void {
+  this.delete.emit(this.user.id);
+}
+
+}
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { User } from '../user.interface';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent {
+  public usersList: User[] = [
+    { id: 1, name: 'Jan', phone: 123123123, email: 'jan@jan' },
+    { id: 2, name: 'Antek', phone: 456456465, email: 'antek@antek' },
+    { id: 3, name: 'Tomek', phone: 789789789, email: 'tomek@tomek' }
+  ];
+
+  onUserDelete(id: number): void {
+    this.usersList = this.usersList.filter(user => id !== user.id);
+  }
+}
