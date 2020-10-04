039cb99fa581b4fd30762405c37851cdc22e92cf
Michał Wajer <wajer.michael@gmail.com>
2020-10-04 11:55:59 +0200

    example directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './user/user.component';
+import { FirstDirectiveDirective } from './first-directive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    FirstDirectiveDirective
   ],
   imports: [
     BrowserModule
