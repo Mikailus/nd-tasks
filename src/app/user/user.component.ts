5d0ff5b31be7de5ed346802de598282584cb6acd
Michał Wajer <wajer.michael@gmail.com>
2020-10-10 13:34:57 +0200

    Formularz users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,20 +1,40 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { User } from '../user.interface';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   @Input() user: User;
   @Input() index: number;
   @Output() delete: EventEmitter<number> = new EventEmitter<number>();
 
-  public today = new Date();
+  public isInEditMode = false;
+  public userFormGroup: FormGroup;
+
+  ngOnInit(): void {
+    this.userFormGroup = new FormGroup({
+      name: new FormControl(this.user.name),
+      phoneNumber: new FormControl(this.user.phone),
+      email: new FormControl(this.user.email)
+    });
+
+    this.userFormGroup.valueChanges.subscribe(console.log);
+  }
 
   onDeleteButtonClick(): void {
     this.delete.emit(this.user.id);
-}
+  }
 
+  toggleEditMode(): void {
+    this.isInEditMode = !this.isInEditMode;
+  }
+
+  onSaveClick(): void {
+    console.log(this.userFormGroup.value);
+    this.toggleEditMode();
+  }
 }
